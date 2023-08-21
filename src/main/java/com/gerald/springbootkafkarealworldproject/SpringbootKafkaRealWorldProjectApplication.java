a4492034ec53c73f68964798650f20c94d271a4d
kamauge <kamaugerald2@gmail.com>
2023-08-21 22:00:32 +0300

    spring boot project implementing kafka with wikimedia events

--- src/main/java/com/gerald/springbootkafkarealworldproject/SpringbootKafkaRealWorldProjectApplication.java
+++ src/main/java/com/gerald/springbootkafkarealworldproject/SpringbootKafkaRealWorldProjectApplication.java
@@ -1,13 +1,13 @@
-package com.example.kafkaspringboot;
+package com.gerald.springbootkafkarealworldproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KafkaSpringbootApplication {
+public class SpringbootKafkaRealWorldProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KafkaSpringbootApplication.class, args);
+		SpringApplication.run(SpringbootKafkaRealWorldProjectApplication.class, args);
 	}
 
 }
